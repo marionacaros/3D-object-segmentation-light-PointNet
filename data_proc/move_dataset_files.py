fccbb799a555971a3165b096f2931ae2f9fa034e
mcr <marionacaros@gmail.com>
2022-05-30 17:30:51 +0200

    update move files

--- data_proc/move_dataset_files.py
+++ data_proc/move_dataset_files.py
@@ -8,152 +8,240 @@ import pickle
 import numpy as np
 import random
 import json
+#
+# # ---- move files to dir ----
+# path =  '/dades/LIDAR/towers_detection/datasets/*/sampled/*pkl'
+# l_files =  glob.glob(path)
+# dest_path = '/dades/LIDAR/towers_detection/datasets/pc_towers_40x40/sampled_2048/'
+#
+# for file in progressbar(l_files):
+#     shutil.move(file, dest_path)
+#
+# path =  '/dades/LIDAR/towers_detection/datasets/*/filtered/*pkl'
+# l_files =  glob.glob(path)
+# dest_path = '/dades/LIDAR/towers_detection/datasets/pc_towers_40x40/data_no_ground/'
+#
+# for file in progressbar(l_files):
+#     shutil.move(file, dest_path)
+#
+# # --------------------------------- DATASET BLOCKS PARTITION  -----------------------------------------------
+# main_path = '/dades/LIDAR/towers_detection/datasets/pc_towers_40x40/sampled_2048/*pkl'
+# list_f = glob.glob(main_path)
+# random.shuffle(list_f)
+# print(len(list_f))
+# print(list_f[0])
+#
+# rib_blocks = {'train':[], 'test':[], 'val':[]}
+# cat3_blocks = {'train':[], 'test':[], 'val':[]}
+# bdn_blocks = {'train':[], 'test':[], 'val':[]}
+#
+# l_tower_files = []
+# l_landscape_files = []
+# i=0
+# for file in progressbar(list_f):
+#
+#     # tower_CAT3_504678_w11.pkl
+#     # tower_RIBERA_pt438656_w18.pkl
+#     # dataset = file.split('_')[-3]
+#     if 'tower20p_' in file:
+#         i+=1
+#         l_tower_files.append(file.split('_')[-2])
+#     else:
+#         l_landscape_files.append(file.split('_')[-2])
+#
+# l_tower_files = set(l_tower_files)
+# l_landscape_files = list(set(l_landscape_files) - l_tower_files)
+#
+# print(f'num total towers: {i}')
+#
+# # towers
+# for i, fileName in enumerate(l_tower_files):
+#
+#     # RIBERA
+#     if 'pt' in fileName:
+#         if fileName not in rib_blocks['test'] and len(rib_blocks['test'])<3:
+#             rib_blocks['test'].append(fileName)
+#         elif len(rib_blocks['val'])<3:
+#             rib_blocks['val'].append(fileName)
+#         else:
+#             rib_blocks['train'].append(fileName)
+#     # BDN
+#     elif 'c' in fileName:
+#         if fileName not in bdn_blocks['test'] and len(bdn_blocks['test'])<1:
+#             bdn_blocks['test'].append(fileName)
+#         elif fileName not in bdn_blocks['val'] and len(bdn_blocks['val'])<1:
+#             bdn_blocks['val'].append(fileName)
+#         else:
+#             bdn_blocks['train'].append(fileName)
+#     # CAT3
+#     else:
+#         if fileName not in cat3_blocks['test'] and len(cat3_blocks['test'])<8:
+#             cat3_blocks['test'].append(fileName)
+#         elif  len(cat3_blocks['val'])<8:
+#             cat3_blocks['val'].append(fileName)
+#         else:
+#             cat3_blocks['train'].append(fileName)
+#
+# print('len landscape files: ',len(l_landscape_files))
+# k=0
+# # no towers files
+# # discarded= l_landscape_files[-150:]
+# for fileName in l_landscape_files:
+#     k+=1
+#     # RIBERA
+#     if 'pt' in fileName:
+#         rib_blocks['train'].append(fileName)
+#     # BDN
+#     elif 'c' in fileName:
+#         bdn_blocks['train'].append(fileName)
+#     # CAT3
+#     else:
+#         cat3_blocks['train'].append(fileName)
+#
+# print('saved landscape files: ',k)
+# print('---CAT3---')
+# print(cat3_blocks['test'])
+# print('---Ribera---')
+# print(rib_blocks['test'])
+# print('---BDN---')
+# print(bdn_blocks['test'])
+#
+# with open('dicts/dataset_blocks_partition_CAT3_reduced' + '.json', 'w') as f:
+#     json.dump(cat3_blocks, f)
+# with open('dicts/dataset_blocks_partition_RIBERA_reduced' + '.json', 'w') as f:
+#     json.dump(rib_blocks, f)
+# with open('dicts/dataset_blocks_partition_BDN_reduced' + '.json', 'w') as f:
+#     json.dump(bdn_blocks, f)
+# # with open('dicts/dataset_blocks_discarded' + '.json', 'w') as f:
+# #     json.dump(discarded, f)
+# #
+# # #  ---------------------------- move files in partitions ---------------------------------------------------------------
+# # #
+#
+# path = '/home/m.caros/work/objectDetection/dicts'
+# with open(path + '/dataset_blocks_partition_CAT3_reduced.json', 'r') as f:
+#     cat3_blocks = json.load(f)
+# with open(path + '/dataset_blocks_partition_RIBERA_reduced.json', 'r') as f:
+#     rib_blocks = json.load(f)
+# with open(path + '/dataset_blocks_partition_BDN_reduced.json', 'r') as f:
+#     bdn_blocks = json.load(f)
+# # with open(path + '/dataset_blocks_discarded.json', 'r') as f:
+# #     discarded = json.load(f)
+#
+#
+# main_path = '/dades/LIDAR/towers_detection/datasets/pc_towers_40x40/data_no_ground/*pkl'
+# dir='filtered'
+# list_f = glob.glob(main_path)
+# print(len(list_f))
+# print(list_f[0])
+#
+# for i, file in enumerate(list_f):
+#     # tower_CAT3_504678_w11.pkl
+#     # tower_RIBERA_pt438656_w18.pkl
+#     dataset = file.split('_')[-3]
+#     fileName = file.split('_')[-2]
+#
+#     if fileName in cat3_blocks['test'] or fileName in rib_blocks['test'] or fileName in bdn_blocks['test']:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/test/'+dir)
+#     elif fileName in cat3_blocks['val'] or fileName in rib_blocks['val'] or fileName in bdn_blocks['val']:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/val/'+dir)
+#     else:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/train/'+dir)
+#
+#
+# main_path = '/dades/LIDAR/towers_detection/datasets/pc_towers_40x40/sampled_2048/*pkl'
+# dir='sampled'
+# list_f = glob.glob(main_path)
+# print(len(list_f))
+# print(list_f[0])
+#
+# for i, file in enumerate(list_f):
+#     # tower_CAT3_504678_w11.pkl
+#     # tower_RIBERA_pt438656_w18.pkl
+#     dataset = file.split('_')[-3]
+#     fileName = file.split('_')[-2]
+#
+#     if fileName in cat3_blocks['test'] or fileName in rib_blocks['test'] or fileName in bdn_blocks['test']:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/test/'+dir)
+#     elif fileName in cat3_blocks['val'] or fileName in rib_blocks['val'] or fileName in bdn_blocks['val']:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/val/'+dir)
+#     else:
+#         shutil.move(file, '/dades/LIDAR/towers_detection/datasets/train/'+dir)
+#
+# # # discard landscape files
+# # for dir in ['filtered', 'sampled']:
+# #
+# #     main_path = '/dades/LIDAR/towers_detection/datasets/train/'+dir+'/*pkl'
+# #     list_f = glob.glob(main_path)
+# #     print(len(list_f))
+# #     print(list_f[0])
+# #
+# #     for i, file in enumerate(list_f):
+# #         # tower_CAT3_504678_w11.pkl
+# #         # tower_RIBERA_pt438656_w18.pkl
+# #         dataset = file.split('_')[-3]
+# #         fileName = file.split('_')[-2]
+# #
+# #         if fileName in discarded:
+# #             shutil.move(file, '/dades/LIDAR/towers_detection/datasets/discarded/'+dir+'/')
+#
 
-# ---- move files to dir ----
-path =  '/dades/LIDAR/towers_detection/datasets/*/sampled/*pkl'
-l_files =  glob.glob(path)
-dest_path = '/dades/LIDAR/towers_detection/datasets/all_sampled/'
-
-for file in progressbar(l_files):
-    shutil.move(file, dest_path)
-
-path =  '/dades/LIDAR/towers_detection/datasets/*/filtered/*pkl'
-l_files =  glob.glob(path)
-dest_path = '/dades/LIDAR/towers_detection/datasets/all_filtered/'
-
-for file in progressbar(l_files):
-    shutil.move(file, dest_path)
-
-# --------------------------------- DATASET BLOCKS PARTITION  -----------------------------------------------
-main_path = '/dades/LIDAR/towers_detection/datasets/all_sampled/*pkl'
-list_f = glob.glob(main_path)
-random.shuffle(list_f)
-print(len(list_f))
-print(list_f[0])
-
-rib_blocks = {'train':[], 'test':[]}
-cat3_blocks = {'train':[], 'test':[]}
-bdn_blocks = {'train':[], 'test':[]}
-
-l_fileNames = []
-l_landscape_files = []
-i=0
-for file in progressbar(list_f):
-
-    # tower_CAT3_504678_w11.pkl
-    # tower_RIBERA_pt438656_w18.pkl
-    # dataset = file.split('_')[-3]
-    if 'tower_' in file:
-        i+=1
-        l_fileNames.append(file.split('_')[-2])
-    else:
-        l_landscape_files.append(file.split('_')[-2])
-
-l_fileNames = set(l_fileNames)
-
-print(f'num total towers: {i}')
-
-# towers
-for i, fileName in enumerate(l_fileNames):
-
-    # RIBERA
-    if 'pt' in fileName:
-        if fileName not in rib_blocks['test'] and len(rib_blocks['test'])<2:
-            rib_blocks['test'].append(fileName)
-        else:
-            rib_blocks['train'].append(fileName)
-    # BDN
-    elif 'c' in fileName:
-        if fileName not in bdn_blocks['test'] and len(bdn_blocks['test'])<1:
-            bdn_blocks['test'].append(fileName)
-        else:
-            bdn_blocks['train'].append(fileName)
-    # CAT3
-    else:
-        if fileName not in cat3_blocks['test'] and len(cat3_blocks['test'])<7:
-            cat3_blocks['test'].append(fileName)
-        else:
-            cat3_blocks['train'].append(fileName)
-
-# no towers files
-for i, fileName in enumerate(l_landscape_files):
-    # RIBERA
-    if 'pt' in fileName:
-        rib_blocks['train'].append(fileName)
-    # BDN
-    elif 'c' in fileName:
-        bdn_blocks['train'].append(fileName)
-    # CAT3
-    else:
-        cat3_blocks['train'].append(fileName)
-
-print('---CAT3---')
-print(cat3_blocks['test'])
-print('---Ribera---')
-print(rib_blocks['test'])
-print('---BDN---')
-print(bdn_blocks['test'])
-
-with open('dicts/dataset_blocks_partition_CAT3' + '.json', 'w') as f:
-    json.dump(cat3_blocks, f)
-with open('dicts/dataset_blocks_partition_RIBERA' + '.json', 'w') as f:
-    json.dump(rib_blocks, f)
-with open('dicts/dataset_blocks_partition_BDN' + '.json', 'w') as f:
-    json.dump(bdn_blocks, f)
-
-# ---------------------------- move files in partitions ---------------------------------------------------------------
-
-for dir in ['filtered', 'sampled']:
-    path = '/home/m.caros/work/objectDetection/dicts'
-    with open( path + '/dataset_blocks_partition_CAT3.json', 'r') as f:
-        cat3_blocks = json.load(f)
-    with open( path + '/dataset_blocks_partition_RIBERA.json', 'r') as f:
-        rib_blocks = json.load(f)
-    with open( path + '/dataset_blocks_partition_BDN.json', 'r') as f:
-        bdn_blocks = json.load(f)
-
-    main_path = '/dades/LIDAR/towers_detection/datasets/all_'+dir+'/*pkl'
-    list_f = glob.glob(main_path)
-    print(len(list_f))
-    print(list_f[0])
-
-    for i, file in enumerate(list_f):
-        # tower_CAT3_504678_w11.pkl
-        # tower_RIBERA_pt438656_w18.pkl
-        dataset = file.split('_')[-3]
-        fileName = file.split('_')[-2]
-
-        if fileName in cat3_blocks['test'] or fileName in rib_blocks['test'] or fileName in bdn_blocks['test']:
-            shutil.move(file, '/dades/LIDAR/towers_detection/datasets/test/'+dir)
-        else:
-            shutil.move(file, '/dades/LIDAR/towers_detection/datasets/train/'+dir)
+# # ------------------------------------ create textfile with names of files --------------------------------
 
 
-# ------------------------------------ create textfile with names of files --------------------------------
-
-for partition in ['train', 'test']:
+for partition in ['val']:
+    f1 = glob.glob(
+        os.path.join('/dades/LIDAR/towers_detection/datasets/', partition, 'sampled', 'tower20p_moved_1*.pkl'))
+    f2 = glob.glob(
+        os.path.join('/dades/LIDAR/towers_detection/datasets/', partition, 'sampled', 'tower20p_moved_2*.pkl'))
+    files = f1 + f2
     print(partition)
-    files = glob.glob(os.path.join('/dades/LIDAR/towers_detection/datasets/',partition,'sampled','*.pkl'))
     print(f'Length files: {len(files)}')
     i_t = 0
     i_l = 0
 
-    with open(partition + '_towers_files.txt', 'w') as f:
+    with open(partition + '_moved_towers_files.txt', 'w') as f:
         for line in files:
-            if 'tower_' in line:
+            if 'tower20p_' in line:
                 file = line.split('/')[-1]
                 f.write(file)
                 f.write('\n')
                 i_t+=1
 
-    with open(partition + '_landscape_files.txt', 'w') as f:
-        for line in files:
-            if not 'tower_' in line:
-                file = line.split('/')[-1]
-                f.write(file)
-                f.write('\n')
-                i_l+=1
+    # with open(partition + '_landscape_files.txt', 'w') as f:
+    #     for line in files:
+    #         if not 'tower20p_' in line:
+    #             file = line.split('/')[-1]
+    #             f.write(file)
+    #             f.write('\n')
+    #             i_l+=1
 
+    print(f'Length towers files: {i_t}')
+    print(f'Length landscape files: {i_l}')
+
+for partition in ['train']:
+    print(partition)
+
+    print(f'Length files: {len(files)}')
+    i_t = 0
+    i_l = 0
+    with open('RGBN_' + partition + '_moved_towers_files.txt', 'w') as f:
+        for line in files:
+            if not 'BDN' in line:
+                if 'tower20p_' in line:
+                    file = line.split('/')[-1]
+                    f.write(file)
+                    f.write('\n')
+                    i_t+=1
+    #
+    # with open('RGBN_' + partition + '_landscape_files.txt', 'w') as f:
+    #     for line in files:
+    #         if not 'BDN' in line:
+    #             if not 'tower20p_' in line:
+    #                 file = line.split('/')[-1]
+    #                 f.write(file)
+    #                 f.write('\n')
+    #                 i_l+=1
     print(f'Length towers files: {i_t}')
     print(f'Length landscape files: {i_l}')
 
